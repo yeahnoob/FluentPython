dff6f2d1e321d244b07c87762f9cf8c937299d81
Xanadu Zhang <teaeml@gmail.com>
2018-09-29 15:29:52 +0800

    Frechdeck example in chapter 01

---
+++ c01_frenchdeck.py
@@ -0,0 +1,19 @@
+import collections
+
+MyCard = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = '梅花 方块 黑桃 红桃'.split()
+
+    def __init__(self):
+        self._cards = [
+            MyCard(rank, suit) for suit in self.suits for rank in self.ranks
+        ]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
